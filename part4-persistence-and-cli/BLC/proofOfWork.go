d9b008ce63d9e08c832f4b12cbf6744e75969f2a
Tucker29 <1173449914@qq.com>
2018-09-25 19:42:13 +0800

    test

--- part4-persistence-and-cli/BLC/proofOfWork.go
+++ part4-persistence-and-cli/BLC/proofOfWork.go
@@ -9,99 +9,48 @@ import (
 )
 
 var (
-	// 定义Nonce最大值
 	maxNonce = math.MaxInt64
 )
 
-const targetBits = 12
-
-// 0000000000000000000001000000000000000000000000000000000000000000000000000000000000000001
-// 00000001
-// 00100000
-// 00010000
-// 8 - 3 = 5
+const targetBit = 12
 
 type ProofOfWork struct {
-	block  *Block   // 当前需要验证的区块
-	target *big.Int // 大数存储,区块难度
+	block  *Block
+	target *big.Int //区块难度
 }
 
-// 数据拼接，返回字节数组
-func (pow *ProofOfWork) prepareData(nonce int) []byte {
-	data := bytes.Join(
-		[][]byte{
-			pow.block.PrevBlockHash,
-			pow.block.Data,
-			IntToHex(pow.block.Timestamp),
-			IntToHex(int64(targetBits)),
-			IntToHex(int64(nonce)),
-		},
-		[]byte{},
-	)
-
-	return data
+func (pow *ProofOfWork) PrepareData(Nonce int) []byte {
+	Data := bytes.Join([][]byte{
+		IntToHex(int64(pow.block.Timestamp)),
+		pow.block.PreHash,
+		pow.block.Data,
+		IntToHex(int64(targetBit)),
+		IntToHex(int64(pow.block.Nonce)),
+	}, []byte{})
+	return Data
 }
 
-// ProofOfWork对象的方法
-func (pow *ProofOfWork) Run() (int, []byte) {
-
-	var hashInt big.Int
-	var hash [32]byte
-	nonce := 0
-	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
-
-	for nonce < maxNonce {
-
-		data := pow.prepareData(nonce)
-		hash = sha256.Sum256(data)
-		fmt.Printf("\r%x", hash)
-		hashInt.SetBytes(hash[:])
-		// pow.target hash
-		// hashInt < pow.target -1
-		// hashInt = pow.target 0
-		// hashInt > pow.target 1
-		// 如果hashInt < pow.target ，break
-		// 000010000000
-		// 000001000000
-		if hashInt.Cmp(pow.target) == -1 {
-			break
-		} else {
-			nonce++
-		}
-	}
-
-	fmt.Println("\n\n")
-
-	return nonce, hash[:]
-}
-
-// 工厂方法
 func NewProofOfWork(block *Block) *ProofOfWork {
-	// 0000000000000000000100000000000000
-	// 00fadadasdasdsfdsf0080808080582452
-	// 000000001111
 	target := big.NewInt(1)
-	//fmt.Println("----------")
-	//fmt.Println(target)
-	target.Lsh(target, uint(256-targetBits))
-	// 0000
-	//fmt.Println(target)
-
+	target.Lsh(target, 256-targetBit)
 	pow := &ProofOfWork{block, target}
-
 	return pow
 }
 
-// 验证当前的工作量证明的有效性
-func (pow *ProofOfWork) Validate() bool {
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var HashInt big.Int
+	var hash [32]byte
+	nonce := 0
+	for nonce < maxNonce {
+		hash := sha256.Sum256(pow.PrepareData(nonce))
+		fmt.Println("\r%x", hash)
+		HashInt.SetBytes(hash[:])
+		if HashInt.Cmp(pow.target) == -1 {
+			break
+		}
 
-	var hashInt big.Int
-
-	data := pow.prepareData(pow.block.Nonce)
-	hash := sha256.Sum256(data)
-	hashInt.SetBytes(hash[:])
-
-	isValid := hashInt.Cmp(pow.target) == -1
-
-	return isValid
+		nonce++
+	}
+	fmt.Println("\n\n")
+	return nonce, hash[:]
 }
